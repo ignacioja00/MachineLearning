a510fea89d5b3e12a02a9f2644a7531e9891ab44
ignacioja1 <ignacioja1@gmail.com>
2022-10-07 15:26:24 +0200

    otra prueba mas

--- Prueba.py
+++ Prueba.py
@@ -5,3 +5,18 @@ def multiplicar(a, b):
 # Función que multiplica 3 números
 def multiplicar3(c,d):
     return c + d    
+
+a = 2
+b = 6    
+c = 5
+d = 10
+
+f = multiplicar(a,b)
+g = multiplicar3(c,d)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(f)
+print(g)
